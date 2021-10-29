3cd378ed7d0976a16e66e1d57ba88cc1503c20c2
Sean Martin <sean@seanmartinstudio.com>
2021-10-29 12:13:33 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+// For Lopp
+function writeCards(names, holiday) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`);
+    }
+    return messages;
+}
+writeCards(["sean", "ashley", "dylan"], "birthday");
+
+// While Loop
+function countDown(number) {
+let counter = 10;
+while (counter >= 0) {
+  console.log(counter--);
+}
+}
+countDown(10);
