b01577f3328020a05367b305ed3f5d52b7b87bb3
Sean Martin <sean@seanmartinstudio.com>
2021-11-13 16:46:04 -0800

    commit tree ae28a5bc3803032b8401eeb8cf0a4921744013b8

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 
-// For Lopp
-function writeCards(names, holiday) {
-    const messages = []
-    for (let i = 0; i < names.length; i++) {
-    messages.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`);
-    }
-    return messages;
-}
-writeCards(["sean", "ashley", "dylan"], "birthday");
+// // For Lopp
+// function writeCards(names, holiday) {
+//     const messages = []
+//     for (let i = 0; i < names.length; i++) {
+//     messages.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`);
+//     }
+//     return messages;
+// }
+// writeCards(["sean", "ashley", "dylan"], "birthday");
 
-// While Loop
-function countDown(number) {
-let counter = 10;
-while (counter >= 0) {
-  console.log(counter--);
+// // While Loop
+// function countDown(number) {
+// let counter = 10;
+// while (counter >= 0) {
+//   console.log(counter--);
+// }
+// }
+// countDown(10);
+
+
+////////////
+
+const stringNames = [];
+
+function writeCards(stringNames, eventNames) {
+  let printStatements = [];
+  for(let i = 0; i < stringNames.length; i++ ) {
+    printStatements.push(`Thank you, ${stringNames[i]}, for the wonderful ${eventNames} gift!`)
+  }
+  return printStatements;
 }
+
+////////////
+
+function countDown() {
+  let i = 10;
+  while (i >= 0) {
+    console.log(i --)
+  }
 }
-countDown(10);
